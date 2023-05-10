c4625c28023e4538177c995463d22a1036601b19
Thinkpad <angelvalverde21@gmail.com>
2023-05-09 22:43:02 -0500

    se agrego la opcion para que se suba la imagen medium

--- resources/views/livewire/manage/products/edit-product/albums/edit-album.blade.php
+++ resources/views/livewire/manage/products/edit-product/albums/edit-album.blade.php
@@ -75,7 +75,7 @@
                 
                             <br /> --}}
 
-                                    <div class="card text-center" style="width: 120px">
+                                    <div class="card text-center" style="width: 140px">
 
                                         <a href="{{  Storage::disk('spaces')->url($image->medium) }}" data-lightbox="show-images-preview"
                                             data-title="{{ $image->label }}">
