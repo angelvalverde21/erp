55feaaceb7f2ba1c9f750a2bf09979e42261d59b
angelvalverde21 <angelvalverde21@gmail.com>
2023-06-07 13:06:17 -0500

    se agrego www.ara.pe al cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -1,42 +1,42 @@
 <?php
 
-namespace App\Http\Middleware;
+// namespace App\Http\Middleware;
 
-use Closure;
+// use Closure;
 
-class Cors
-{
-    public function handle($request, Closure $next)
-    {
-        $allowedDomains = [
-            'https://ara.pe',
-            'https://www.ara.pe',
-            'http://ara.pe',
-            'http://www.ara.pe',
-        ];
+// class Cors
+// {
+//     public function handle($request, Closure $next)
+//     {
+//         $allowedDomains = [
+//             'https://ara.pe',
+//             'https://www.ara.pe',
+//             'http://ara.pe',
+//             'http://www.ara.pe',
+//         ];
 
-        $origin = $request->header('Origin');
+//         $origin = $request->header('Origin');
         
-        if (in_array($origin, $allowedDomains)) {
-            $headers = [
-                'Access-Control-Allow-Origin' => $origin,
-                'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE',
-                'Access-Control-Allow-Headers' => 'Content-Type, X-Requested-With',
-            ];
-        } else {
-            $headers = [];
-        }
+//         if (in_array($origin, $allowedDomains)) {
+//             $headers = [
+//                 'Access-Control-Allow-Origin' => $origin,
+//                 'Access-Control-Allow-Methods' => 'GET, POST, PUT, DELETE',
+//                 'Access-Control-Allow-Headers' => 'Content-Type, X-Requested-With',
+//             ];
+//         } else {
+//             $headers = [];
+//         }
 
-        if ($request->isMethod('OPTIONS')) {
-            return response()->json(['method' => 'OPTIONS'], 200, $headers);
-        }
+//         if ($request->isMethod('OPTIONS')) {
+//             return response()->json(['method' => 'OPTIONS'], 200, $headers);
+//         }
 
-        $response = $next($request);
+//         $response = $next($request);
         
-        foreach ($headers as $key => $value) {
-            $response->header($key, $value);
-        }
+//         foreach ($headers as $key => $value) {
+//             $response->header($key, $value);
+//         }
 
-        return $response;
-    }
-}
+//         return $response;
+//     }
+// }
