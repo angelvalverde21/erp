d0b483d517c1c2a0c811ec5f96602668b956e246
angelvalverde21 <angelvalverde21@gmail.com>
2023-06-09 21:38:53 -0500

    se agrego el boton descargar stock en el editproduct

--- resources/views/livewire/manage/orders/edit-order/_navbar-delivery-man.blade.php
+++ resources/views/livewire/manage/orders/edit-order/_navbar-delivery-man.blade.php
@@ -1,8 +1,10 @@
+   {{ $order->store->repartidores($order->store_id) }}
+
 <x-form.select label="Entregado por" wirevalue="order.delivery_man_id" icon="fa-solid fa-person-biking"
     wirechange="saveSelected()">
     <option value="0">Seleccionar</option>
 
-    {{ $order->store }}
+ 
 
     @foreach ($order->store->repartidores($order->store_id) as $repartidor)
         <option value="{{ $repartidor->id }}">{{ $repartidor->name }}
